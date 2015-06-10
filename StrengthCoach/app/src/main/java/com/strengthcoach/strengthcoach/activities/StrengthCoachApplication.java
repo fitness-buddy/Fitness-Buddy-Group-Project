b329d554f5bc5e4ee2e718278d19042d949b9a78
varun <varungupta@live.com>
2015-06-09 21:48:02 -0700

    Add push notification.

--- StrengthCoach/app/src/main/java/com/strengthcoach/strengthcoach/activities/StrengthCoachApplication.java
+++ StrengthCoach/app/src/main/java/com/strengthcoach/strengthcoach/activities/StrengthCoachApplication.java
@@ -1,9 +1,13 @@
 package com.strengthcoach.strengthcoach.activities;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 import com.strengthcoach.strengthcoach.models.Address;
 import com.strengthcoach.strengthcoach.models.Gym;
 import com.strengthcoach.strengthcoach.models.Message;
@@ -28,5 +32,16 @@ public class StrengthCoachApplication extends Application {
         ParseObject.registerSubclass(TrainerSlots.class);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 }
