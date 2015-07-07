ffe4da561a2c3e10706b51f6d1d6c21a0c1e15f3
Nyjal Augustine <nyjal@box.com>
2015-07-06 23:14:04 -0700

    Use new drawables for launcher activity Ken burns effect in the launch screen Hide status bar and navigation bar using the immersive sticky apporach

--- StrengthCoach/app/src/main/java/com/strengthcoach/strengthcoach/activities/LauncherActivity.java
+++ StrengthCoach/app/src/main/java/com/strengthcoach/strengthcoach/activities/LauncherActivity.java
@@ -1,13 +1,9 @@
 package com.strengthcoach.strengthcoach.activities;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,25 +11,46 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.strengthcoach.strengthcoach.R;
 
 public class LauncherActivity extends ActionBarActivity {
     Button bLogin;
     ImageView ivBackground;
     TextView tvSkip;
+    View decorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_launcher);
         setupViews();
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+
     public void setupViews(){
-        ivBackground = (ImageView)findViewById(R.id.ivBackground);
+        ivBackground = (KenBurnsView)findViewById(R.id.ivBackground);
         ivBackground.setImageResource(R.drawable.slide_animation);
         AnimationDrawable frameAnimation = (AnimationDrawable) ivBackground.getDrawable();
-        frameAnimation.setEnterFadeDuration(3000);
-        frameAnimation.setExitFadeDuration(3000);
+        frameAnimation.setEnterFadeDuration(2000);
+        frameAnimation.setExitFadeDuration(2000);
         frameAnimation.start();
 
         tvSkip = (TextView) findViewById(R.id.tvSkip);
